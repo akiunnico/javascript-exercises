387c0d8ef0ff5f72a5efc733204cd8c7c5725313
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-10-05 02:39:11 +0200

    Finish two first javascript-exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num<0) {
+        return 'ERROR';
+    }else{
+        let result = '';
+        for(let i=1; i<=num; i++){
+        result += string;
+    }
+        return result;
+    }
+    
 };
-
+const number = Math.floor(Math.random() * 1000)
+repeatString('hey',3);
+repeatString('hello',10);
+repeatString('hi',1);
+repeatString('bye',0);
+repeatString('goodbye',-1);
+repeatString('odin',number);
+repeatString('', 10);
 // Do not edit below this line
 module.exports = repeatString;
