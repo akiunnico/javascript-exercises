8a0afc6aabc61c8698b77cbac16387be0bfc6d79
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-05 02:36:28 +0100

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let fraseFinal = "";
+    if(num < 0) {return "ERROR";} 
+    for(let i = 1; i <= num; i++){
+        fraseFinal += string;
+    }
+    return fraseFinal;
 };
-
+console.log(repeatString("hey", 3))
 // Do not edit below this line
 module.exports = repeatString;
