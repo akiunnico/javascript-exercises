79459576da5f26c66f9854b146c9fe993e93c36a
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-05 17:32:58 +0100

    Finsh exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let suma = 0;
+    let menor, mayor;
+    if(int1 < 0 || int2 < 0) return "ERROR";
+    if(!(Number.isInteger(int1) && Number.isInteger(int2))) return "ERROR";
+    if(typeof(int1) !== "number" || typeof(int2) !== "number") return "ERROR";
+    if(int1 > int2)
+        {menor = int2; mayor = int1}
+    else{menor = int1; mayor = int2}
+    for(let i = menor; i <= mayor; i++){
+        suma += i;
+    }
+    return suma;
 };
 
+console.log(sumAll(10, [90, 1]));
 // Do not edit below this line
 module.exports = sumAll;
