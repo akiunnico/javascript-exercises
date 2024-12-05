20ddb2e33ba2dedd3d6e8aef4b9b48f1e255543d
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-05 03:10:02 +0100

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(texto) {
+    return textoDelReves = texto.split("").reverse().join("");
 };
+
+console.log(reverseString('hello'));
 
 // Do not edit below this line
 module.exports = reverseString;
