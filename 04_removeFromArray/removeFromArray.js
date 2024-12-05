1d890dc614b3dfba597c5b27133ae6758a929edf
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-05 12:12:01 +0100

    Finisch exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+      for(argument of arguments){
+        borrados = array.filter(e => !args.includes(e));
+      }
+    return borrados;
+    };
+console.log(removeFromArray([1, 2, 2, 3], 2))
 
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
