81ee29c97b7abbb4b49ff4fa6e4e8cc9e5cb2d61
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-05 18:45:01 +0100

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  celsius = (temp - 32) * 5 / 9;
+  return (Number.isInteger(celsius))
+    ? celsius
+    : parseFloat( celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  farenheit = temp * 9 / 5 + 32;
+  return (Number.isInteger(farenheit))
+    ? farenheit
+    : parseFloat( farenheit.toFixed(1));
 };
+
+console.log(convertToCelsius(100));
+
+console.log(convertToFahrenheit(-10));
 
 // Do not edit below this line
 module.exports = {
