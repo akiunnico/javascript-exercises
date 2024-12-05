9cd3d929a4d6def931625e541c034906aab88572
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-05 21:52:38 +0100

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,41 @@
-const add = function() {
-	
+const add = function(...args) {
+  let resultado = 0;
+  for(num of args) resultado += num;
+	return resultado;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+
+	return args[0]-args[1];
 };
 
-const sum = function() {
-	
+
+const sum = function(array) {
+	return array.reduce((total, current) => total += current, 0)
 };
 
-const multiply = function() {
+
+const multiply = function(array) {
+  
+  return array.reduce((total, current) => total *= current, 1)
 
 };
 
-const power = function() {
-	
+
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
-};
 
+const factorial = function(num) {
+	if(num === 0) return 1;
+  let resultado=1;
+  for(let i=1; i <= num; i++){
+    resultado *= i;
+  }
+  return resultado;
+};
+console.log(factorial(10));
 // Do not edit below this line
 module.exports = {
   add,
