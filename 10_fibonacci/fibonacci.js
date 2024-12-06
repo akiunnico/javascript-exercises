4f3583a8a32e4fd94281ec1d57900c4beb5bd0e6
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-06 15:53:06 +0100

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(posicion) {
+    posicion = parseInt(posicion);
+    if(posicion < 0) return "OOPS";
+    let serie = [0, 1];
+    for(let i = 2; i <= posicion; i++){
+        serie[i] = serie[i-1] + serie[i-2]
+        // console.log(serie[i])
+    }
+    return serie[posicion];
 };
 
+console.log(fibonacci("8"));
 // Do not edit below this line
 module.exports = fibonacci;
