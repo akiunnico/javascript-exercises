86d3e671eeb3444395dde97e54f9c7901b01628d
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-06 14:21:10 +0100

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (texto) {
+
+    let miMuestra = "abcdefghijklmnñopqrstuvwxyz0123456789";
+    let filtrado = texto.toLowerCase().split("").filter(e => miMuestra.includes(e));
+    let desnudo = filtrado.join("");
+    let invertido = filtrado.reverse().join("");
+    console.log(`Desnudo: ${desnudo} Invertido: ${invertido}`)
+    return desnudo === invertido ? true : false; 
 
 };
+
+let conejillo = 'r3ace3car';
+
+console.log(palindromes(conejillo));
+
+// console.log(conejillo.split(""));
 
 // Do not edit below this line
 module.exports = palindromes;
