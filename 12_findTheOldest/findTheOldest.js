ae6e07781c61da60d047bad39e17de71c7f38b8d
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-07 11:45:22 +0100

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  array.forEach(element => {
+    if(element.yearOfDeath === undefined){element.yearOfDeath = new Date().getFullYear()}
+  });
+  const sorted = array.sort((a, b)=> {
+    const lastGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
 
+    return lastGuy > nextGuy ? -1 : 1;
+  })
+  return sorted[0];
 };
 
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+  ]
+  console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
