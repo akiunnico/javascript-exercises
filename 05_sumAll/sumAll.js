a3580c5d6c9db30ff859ef975412e6444b62c7e0
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-07 17:00:43 +0100

    Inspect javascript exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,7 +3,6 @@ const sumAll = function(int1, int2) {
     let menor, mayor;
     if(int1 < 0 || int2 < 0) return "ERROR";
     if(!(Number.isInteger(int1) && Number.isInteger(int2))) return "ERROR";
-    if(typeof(int1) !== "number" || typeof(int2) !== "number") return "ERROR";
     if(int1 > int2)
         {menor = int2; mayor = int1}
     else{menor = int1; mayor = int2}
